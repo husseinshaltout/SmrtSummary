f4a2f17b7e6a3052a19000bdf109796407e325d1
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2020-03-15 06:59:44 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,12 +2,12 @@
 """
 Created on Sat Mar 14 06:32:54 2020
 
-@author: Hussein
+@author: Dell
 """
 from SmrtSummary import SmrtSummary
 import glob
-import sys
-import os
+
+
 from PIL import Image, ImageTk
 try:
     import Tkinter as tk
@@ -22,6 +22,8 @@ def vp_start_gui():
 
 
 class Toplevel1:
+    #SmrtSummary object                  
+    ss = SmrtSummary()   
     def __init__(self, top=None):
 
         top.geometry("710x450+333+128")
@@ -29,17 +31,17 @@ class Toplevel1:
         top.maxsize(1364, 749)
         top.resizable(1, 1)
         top.title("SmrtSummary")
-        top.configure(background="#000000")
+        top.configure(background="#d9d9d9")
 
  
         #Canvas showing summary image           
         self.canvas = tk.Canvas(top)
         self.canvas.place(relx=0.028, rely=0.022, relwidth=0.5, relheight=0.878)    
-        # self.img = Image.open("summary.png")
-        # self.img = self.img.resize((157, 1080), Image.ANTIALIAS)
-        # self.img = ImageTk.PhotoImage(self.img)  
-        # self.canvas.create_image(10,10, image=self.img)  
-        # self.canvas.image = self.img   
+        self.img = Image.open("summary.png")
+        self.img = self.img.resize((157, 1080), Image.ANTIALIAS)
+        self.img = ImageTk.PhotoImage(self.img)  
+        self.canvas.create_image(10,10, image=self.img)  
+        self.canvas.image = self.img   
 
         #Listbox showing all frames
         self.Scrolledlistbox1 = tk.Listbox(top)       
@@ -48,31 +50,31 @@ class Toplevel1:
         self.scrollbar.pack( side = tk.RIGHT, fill = tk.Y )       
         self.Scrolledlistbox1.configure(yscrollcommand=self.scrollbar.set)
         #double click on item in listbox to show item on small canvas        
-        self.Scrolledlistbox1.bind("<Double-Button-1>", self.C2_showimg)
+        self.Scrolledlistbox1.bind("<Double-Button-1>", self.showimg)
         #loop to show files in listbox    
         self.path = 'frames'
         self.fpath = glob.glob('%s\\*.jpg'%self.path)
+
+
         #Small canvas to show frames
         self.S_canvas = tk.Canvas(top)
         self.S_canvas.place(relx=0.720, rely=0.022, relheight=0.250, relwidth=0.250)
-        #SmrtSummary object                  
-        ss = SmrtSummary()   
         #Split Video button
-        self.Button2 = tk.Button(top,command=lambda:[ss.split(),self.fList()])
+        self.Button2 = tk.Button(top,command=lambda:[self.fList()])
         self.Button2.place(relx=0.803, rely=0.3, height=24, width=85)
         self.Button2.configure(text='''Split video''')
         
         #Scanline Crop
-        self.Button_scan = tk.Button(top,command=lambda:ss.scanline())
+        self.Button_scan = tk.Button(top,command=lambda:self.ss.scanline)
         self.Button_scan.place(relx=0.803, rely=0.4, height=24, width=85)
         self.Button_scan.configure(text='''Scanline crop''')
         
         #Get Summary Button
-        self.Button1 = tk.Button(top,command=lambda:[ss.scanline(),ss.image_Summary(),C1_showimg()])
+        self.Button1 = tk.Button(top,command=lambda:self.ss.image_Summary)
         self.Button1.place(relx=0.803, rely=0.5, height=24, width=85)
         self.Button1.configure(text='''Get summary''')
-    #Show image in small canavas
-    def C2_showimg(self,z):
+
+    def showimg(self,z):
         x = self.Scrolledlistbox1.get(tk.ANCHOR)
         self.img = Image.open("frames/%s"%x)
         self.img = self.img.resize((400, 200), Image.ANTIALIAS)
@@ -80,18 +82,28 @@ class Toplevel1:
         self.S_canvas.create_image(0,0, image=self.img, anchor="nw")  
         self.S_canvas.image = self.img  
         print(x,z)
-    def C1_showimg(self,z):
-        self.img = Image.open("summary.png")
-        self.img = self.img.resize((157, 1080), Image.ANTIALIAS)
-        self.img = ImageTk.PhotoImage(self.img)  
-        self.canvas.create_image(10,10, image=self.img)  
-        self.canvas.image = self.img  
-        print("added to canvas")
-    #loop to show files in listbox           
+        
+    #loop to show files in listbox          
     def fList(self):
         for x in range(len(self.fpath)):
             self.Scrolledlistbox1.insert(tk.END, "frame{0}.jpg".format(x))
-
+            
+    def C2_showimg(self,z):
+            x = self.Scrolledlistbox1.get(tk.ANCHOR)
+            self.img = Image.open("frames/%s"%x)
+            self.img = self.img.resize((400, 200), Image.ANTIALIAS)
+            
+    def C1_showimg(self,z):
+            self.img = Image.open("summary.png")
+            self.img = self.img.resize((157, 1080), Image.ANTIALIAS)
+            self.img = ImageTk.PhotoImage(self.img)  
+            self.canvas.create_image(10,10, image=self.img)  
+            self.canvas.image = self.img  
+            print("added to canvas")  
+    def fun1(self):
+        self.ss.split()
+        self.fList()
+        
 if __name__ == '__main__':
     vp_start_gui()
 
