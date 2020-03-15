e63b82d0658b3f3dd4a8fcc50f37f36a85419667
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2020-03-15 03:11:27 +0200

    Update SmrtSummary.py

--- SmrtSummary.py
+++ SmrtSummary.py
@@ -17,7 +17,7 @@ class SmrtSummary:
         #cropped files location
         self.C_path = '%s/cropped'%self.path
         #All files with extension jpeg in frames folder
-        self.fpath = glob.glob('C:\\Users\\Dell\\Documents\\GitHub\\SmrtSummary\\frames\\*.jpg')
+        self.fpath = glob.glob('%s\\*.jpg'%self.path)
     #Function to split video into frames
     def split(self):
         i=0
@@ -31,19 +31,23 @@ class SmrtSummary:
         self.cap.release()
         cv.destroyAllWindows()
         
-    def readLine(self): 
+    def scanline(self): 
         #Crop all frames with defined x value/line
         for filename in self.fpath:
             im = cv.imread(filename,cv.IMREAD_COLOR)
             rows = im.shape[0]        
-            cropped = im[0:rows, 1069:1070]             
+            cropped = im[0:rows, 1069:1070]  
+            print(filename.split("\\")[-1])
             cv.imwrite(os.path.join(self.C_path , filename.split("\\")[-1]), cropped)
-        #Concatenate all cropped images horizontaly 
+    
+    def image_Summary(self):
+        # Concatenate all cropped images horizontaly 
         for x in range(len(self.fpath)):
+            print(os.path.join(self.C_path ,"frame{0}.jpg").format(x))
             if(x == 0):
-                numpy_horizontal = cv.imread("%s\\frame{0}.jpg"%self.C_path.format(x))
+                numpy_horizontal = cv.imread(os.path.join(self.C_path ,"frame{0}.jpg").format(x))
             else:
-                img = cv.imread("%s\\frame{0}.jpg"%self.C_path.format(x))
+                img = cv.imread(os.path.join(self.C_path ,"frame{0}.jpg").format(x))
                 numpy_horizontal = np.hstack((numpy_horizontal, img))
         #Create summary image
         cv.imwrite("summary.png", numpy_horizontal)
@@ -51,7 +55,7 @@ class SmrtSummary:
     def mvideo(self):
         img_array = []
         for x in range(len(self.fpath)):
-            img = cv.imread("C:\\Users\\Dell\\Documents\\GitHub\\SmrtSummary\\frames\\cropped\\frame{0}.jpg".format(x))
+            img = cv.imread(os.path.join(self.C_path ,"frame{0}.jpg").format(x))
             height, width, layers = img.shape
             size = (width,height)
             img_array.append(img)
@@ -68,7 +72,8 @@ class SmrtSummary:
         cv.destroyWindow(winname)        
     def start(self):
         # self.split()
-        # self.readLine()
+        self.scanline()
+        # self.image_Summary()
         # self.show_wait_destroy("test", self.readLine())
 if __name__ == "__main__":
     SmrtSummary = SmrtSummary()        
