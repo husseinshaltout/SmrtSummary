6df2be6987c6738e9353115510823242fc05441a
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2020-03-15 05:20:04 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -13,52 +13,15 @@ try:
 except ImportError:
     import tkinter as tk
 
-try:
-    import ttk
-    py3 = False
-except ImportError:
-    import tkinter.ttk as ttk
-    py3 = True
-
-
 def vp_start_gui():
     '''Starting point when module is the main routine.'''
-    global val, w, root
     root = tk.Tk()
     top = Toplevel1 (root)
     root.mainloop()
 
-w = None
-def create_Toplevel1(root, *args, **kwargs):
-    '''Starting point when module is imported by another program.'''
-    global w, w_win, rt
-    rt = root
-    w = tk.Toplevel (root)
-    top = Toplevel1 (w)
-    return (w, top)
-
-def destroy_Toplevel1():
-    global w
-    w.destroy()
-    w = None
 
 class Toplevel1:
     def __init__(self, top=None):
-        '''This class configures and populates the toplevel window.
-           top is the toplevel containing window.'''
-        _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
-        _fgcolor = '#000000'  # X11 color: 'black'
-        _compcolor = '#d9d9d9' # X11 color: 'gray85'
-        _ana1color = '#d9d9d9' # X11 color: 'gray85'
-        _ana2color = '#ececec' # Closest X11 color: 'gray92'
-        self.style = ttk.Style()
-        if sys.platform == "win32":
-            self.style.theme_use('winnative')
-        self.style.configure('.',background=_bgcolor)
-        self.style.configure('.',foreground=_fgcolor)
-        self.style.configure('.',font="TkDefaultFont")
-        self.style.map('.',background=
-            [('selected', _compcolor), ('active',_ana2color)])
 
         top.geometry("710x450+333+128")
         top.minsize(120, 1)
@@ -68,7 +31,7 @@ class Toplevel1:
         top.configure(background="#d9d9d9")
 
  
-        
+        #Canvas showing summary image           
         self.canvas = tk.Canvas(top)
         self.canvas.place(relx=0.028, rely=0.022, relwidth=0.5, relheight=0.878)    
         self.img = Image.open("summary.png")
@@ -76,74 +39,41 @@ class Toplevel1:
         self.img = ImageTk.PhotoImage(self.img)  
         self.canvas.create_image(10,10, image=self.img)  
         self.canvas.image = self.img   
-        
-        # img = Image.open("summary.png")
-        # img = img.resize((250, 250), Image.ANTIALIAS)
-        # img = ImageTk.PhotoImage(img)
 
-
-
-        
+        #Listbox showing all frames
         self.Scrolledlistbox1 = tk.Listbox(top)       
         self.Scrolledlistbox1.place(relx=0.563, rely=0.022, relheight=0.878, relwidth=0.142)
-        
         self.scrollbar = tk.Scrollbar(top,command=self.Scrolledlistbox1.yview)
         self.scrollbar.pack( side = tk.RIGHT, fill = tk.Y )       
-        self.Scrolledlistbox1.configure(yscrollcommand=self.scrollbar.set)        
+        self.Scrolledlistbox1.configure(yscrollcommand=self.scrollbar.set)
+        #double click on item in listbox to show item on small canvas        
         self.Scrolledlistbox1.bind("<Double-Button-1>", self.showimg)
-        
-
-        
-        
-        
+        #loop to show files in listbox    
         self.path = 'frames'
         self.fpath = glob.glob('%s\\*.jpg'%self.path)
         for x in range(len(self.fpath)):
             self.Scrolledlistbox1.insert(tk.END, "frame{0}.jpg".format(x))
-            # os.path.join(self.fpath ,"frame{0}.jpg").format(x)
 
 
-
+        #Small canvas to show frames
         self.S_canvas = tk.Canvas(top)
         self.S_canvas.place(relx=0.720, rely=0.022, relheight=0.250, relwidth=0.250)                  
             
-
-        self.Button1 = tk.Button(top)
-        self.Button1.place(relx=0.803, rely=0.5, height=24, width=85)
-        self.Button1.configure(activebackground="#ececec")
-        self.Button1.configure(activeforeground="#000000")
-        self.Button1.configure(background="#d9d9d9")
-        self.Button1.configure(disabledforeground="#a3a3a3")
-        self.Button1.configure(foreground="#000000")
-        self.Button1.configure(highlightbackground="#d9d9d9")
-        self.Button1.configure(highlightcolor="black")
-        self.Button1.configure(pady="0")
-        self.Button1.configure(text='''Get summary''')
-
+        #Split Video button
         self.Button2 = tk.Button(top)
         self.Button2.place(relx=0.803, rely=0.3, height=24, width=85)
-        self.Button2.configure(activebackground="#ececec")
-        self.Button2.configure(activeforeground="#000000")
-        self.Button2.configure(background="#d9d9d9")
-        self.Button2.configure(disabledforeground="#a3a3a3")
-        self.Button2.configure(foreground="#000000")
-        self.Button2.configure(highlightbackground="#d9d9d9")
-        self.Button2.configure(highlightcolor="black")
-        self.Button2.configure(pady="0")
         self.Button2.configure(text='''Split video''')
         
+        #Scanline Crop
         self.Button_scan = tk.Button(top)
         self.Button_scan.place(relx=0.803, rely=0.4, height=24, width=85)
-        self.Button_scan.configure(activebackground="#ececec")
-        self.Button_scan.configure(activeforeground="#000000")
-        self.Button_scan.configure(background="#d9d9d9")
-        self.Button_scan.configure(disabledforeground="#a3a3a3")
-        self.Button_scan.configure(foreground="#000000")
-        self.Button_scan.configure(highlightbackground="#d9d9d9")
-        self.Button_scan.configure(highlightcolor="black")
-        self.Button_scan.configure(pady="0")
         self.Button_scan.configure(text='''Scanline crop''')
         
+        #Get Summary Button
+        self.Button1 = tk.Button(top)
+        self.Button1.place(relx=0.803, rely=0.5, height=24, width=85)
+        self.Button1.configure(text='''Get summary''')
+
     def showimg(self,z):
         x = self.Scrolledlistbox1.get(tk.ANCHOR)
         self.img = Image.open("frames/%s"%x)
@@ -153,129 +83,6 @@ class Toplevel1:
         self.S_canvas.image = self.img  
         print(x,z)
 
-# The following code is added to facilitate the Scrolled widgets you specified.
-class AutoScroll(object):
-    '''Configure the scrollbars for a widget.'''
-
-    def __init__(self, master):
-        #  Rozen. Added the try-except clauses so that this class
-        #  could be used for scrolled entry widget for which vertical
-        #  scrolling is not supported. 5/7/14.
-        try:
-            vsb = ttk.Scrollbar(master, orient='vertical', command=self.yview)
-        except:
-            pass
-        hsb = ttk.Scrollbar(master, orient='horizontal', command=self.xview)
-
-        #self.configure(yscrollcommand=_autoscroll(vsb),
-        #    xscrollcommand=_autoscroll(hsb))
-        try:
-            self.configure(yscrollcommand=self._autoscroll(vsb))
-        except:
-            pass
-        self.configure(xscrollcommand=self._autoscroll(hsb))
-
-        self.grid(column=0, row=0, sticky='nsew')
-        try:
-            vsb.grid(column=1, row=0, sticky='ns')
-        except:
-            pass
-        hsb.grid(column=0, row=1, sticky='ew')
-
-        master.grid_columnconfigure(0, weight=1)
-        master.grid_rowconfigure(0, weight=1)
-
-        # Copy geometry methods of master  (taken from ScrolledText.py)
-        if py3:
-            methods = tk.Pack.__dict__.keys() | tk.Grid.__dict__.keys() \
-                  | tk.Place.__dict__.keys()
-        else:
-            methods = tk.Pack.__dict__.keys() + tk.Grid.__dict__.keys() \
-                  + tk.Place.__dict__.keys()
-
-        for meth in methods:
-            if meth[0] != '_' and meth not in ('config', 'configure'):
-                setattr(self, meth, getattr(master, meth))
-
-    @staticmethod
-    def _autoscroll(sbar):
-        '''Hide and show scrollbar as needed.'''
-        def wrapped(first, last):
-            first, last = float(first), float(last)
-            if first <= 0 and last >= 1:
-                sbar.grid_remove()
-            else:
-                sbar.grid()
-            sbar.set(first, last)
-        return wrapped
-
-    def __str__(self):
-        return str(self.master)
-
-def _create_container(func):
-    '''Creates a ttk Frame with a given master, and use this new frame to
-    place the scrollbars and the widget.'''
-    def wrapped(cls, master, **kw):
-        container = ttk.Frame(master)
-        container.bind('<Enter>', lambda e: _bound_to_mousewheel(e, container))
-        container.bind('<Leave>', lambda e: _unbound_to_mousewheel(e, container))
-        return func(cls, container, **kw)
-    return wrapped
-
-class ScrolledListBox(AutoScroll, tk.Listbox):
-    '''A standard Tkinter Listbox widget with scrollbars that will
-    automatically show/hide as needed.'''
-    @_create_container
-    def __init__(self, master, **kw):
-        tk.Listbox.__init__(self, master, **kw)
-        AutoScroll.__init__(self, master)
-    def size_(self):
-        sz = tk.Listbox.size(self)
-        return sz
-
-import platform
-def _bound_to_mousewheel(event, widget):
-    child = widget.winfo_children()[0]
-    if platform.system() == 'Windows' or platform.system() == 'Darwin':
-        child.bind_all('<MouseWheel>', lambda e: _on_mousewheel(e, child))
-        child.bind_all('<Shift-MouseWheel>', lambda e: _on_shiftmouse(e, child))
-    else:
-        child.bind_all('<Button-4>', lambda e: _on_mousewheel(e, child))
-        child.bind_all('<Button-5>', lambda e: _on_mousewheel(e, child))
-        child.bind_all('<Shift-Button-4>', lambda e: _on_shiftmouse(e, child))
-        child.bind_all('<Shift-Button-5>', lambda e: _on_shiftmouse(e, child))
-
-def _unbound_to_mousewheel(event, widget):
-    if platform.system() == 'Windows' or platform.system() == 'Darwin':
-        widget.unbind_all('<MouseWheel>')
-        widget.unbind_all('<Shift-MouseWheel>')
-    else:
-        widget.unbind_all('<Button-4>')
-        widget.unbind_all('<Button-5>')
-        widget.unbind_all('<Shift-Button-4>')
-        widget.unbind_all('<Shift-Button-5>')
-
-def _on_mousewheel(event, widget):
-    if platform.system() == 'Windows':
-        widget.yview_scroll(-1*int(event.delta/120),'units')
-    elif platform.system() == 'Darwin':
-        widget.yview_scroll(-1*int(event.delta),'units')
-    else:
-        if event.num == 4:
-            widget.yview_scroll(-1, 'units')
-        elif event.num == 5:
-            widget.yview_scroll(1, 'units')
-
-def _on_shiftmouse(event, widget):
-    if platform.system() == 'Windows':
-        widget.xview_scroll(-1*int(event.delta/120), 'units')
-    elif platform.system() == 'Darwin':
-        widget.xview_scroll(-1*int(event.delta), 'units')
-    else:
-        if event.num == 4:
-            widget.xview_scroll(-1, 'units')
-        elif event.num == 5:
-            widget.xview_scroll(1, 'units')
 
 if __name__ == '__main__':
     vp_start_gui()
