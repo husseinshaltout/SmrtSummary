be0bde72c007a0d934e6a40beba786566b69da47
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2022-06-10 13:48:35 +0200

    updated settings

--- SmrtSummary/settings.py
+++ SmrtSummary/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['smrtsummary.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['smrtsummary.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'SmrtSummary/static')
 ]
