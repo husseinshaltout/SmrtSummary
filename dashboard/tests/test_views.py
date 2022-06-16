ef9aebddfbf8d886108a9980479bda11469cb2fa
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2022-06-16 13:19:12 +0200

    test: added unit tests for dashboard views upload function

--- dashboard/tests/test_views.py
+++ dashboard/tests/test_views.py
@@ -1,1 +1,59 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+from dashboard.forms import VideoForm
+
+
+class BaseTest(TestCase):
+    def setUp(self) -> None:
+        self.registerURL = reverse("register")
+        self.uploadURL = reverse("upload")
+        self.scanlineURL = "/dashboard/2"
+        self.user = {
+            "firstname": "TestFirstName",
+            "lastname": "TestLastName",
+            "username": "username",
+            "email": "username@email.com",
+            "password": "password",
+            "password2": "password",
+        }
+        self.videoFile = SimpleUploadedFile(
+            "test.mp4",
+            open("C:/Users/Dell/Documents/GitHub/SmrtSummary/test.mp4", "rb").read(),
+            content_type="video/mp4",
+        )
+
+        return super().setUp()
+
+
+class UploadTest(BaseTest):
+    def test_can_view_page_correctly(self) -> None:
+        self.client.post(self.registerURL, self.user, format="text/html")
+        response = self.client.get(self.uploadURL)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "dashboard/upload.html")
+
+    def test_can_upload_file(self) -> None:
+        self.client.post(self.registerURL, self.user, format="text/html")
+        response = self.client.post(self.uploadURL, {"videofile": self.videoFile})
+        self.assertRedirects(response, self.scanlineURL)
+
+    def test_upload_page_uses_video_form(self) -> None:
+        self.client.post(self.registerURL, self.user, format="text/html")
+        response = self.client.get(self.uploadURL)
+        self.assertIsInstance(response.context["form"], VideoForm)
+
+    # def test_video_form(self) -> None:
+    #     self.client.post(self.registerURL, self.user, format="text/html")
+    #     # user = User.objects.filter(email=self.user['email']).first()
+    #     # response = self.client.post(self.uploadURL,
+    #     #                             {'video': self.videoFile})
+    #     request = HttpRequest()
+    #     request.POST = {"videofile": self.videoFile}
+    #     form = VideoForm(request.POST)
+    #     self.assertTrue(form.is_valid())
+
+    # def test_unauthorized_user_scanline_video_access(self) -> None:
+    #     response = self.client.get(self.scanlineURL, format="text/html")
+    #     print(response.content)
+    #     self.assertHTMLEqual(response.content, "<h1>Not authenticated</h1>")
--- dashboard/views.py
+++ dashboard/views.py
@@ -9,27 +9,24 @@ from django.contrib.auth.decorators import login_required
 from django.core.files import File
 
 
-@login_required(login_url='/accounts/login', redirect_field_name='')
+@login_required(login_url="/accounts/login", redirect_field_name="")
 def home(request):
-    videos = Video.objects.order_by(
-                                    '-upload_date'
-                                    ).filter(uploaded_by=request.user)
+    videos = Video.objects.order_by("-upload_date").filter(uploaded_by=request.user)
     context = {
-        'uploaded_videos': videos,
+        "uploaded_videos": videos,
     }
-    return render(request, 'dashboard/video_listings.html', context)
+    return render(request, "dashboard/video_listings.html", context)
 
 
-@login_required(login_url='/accounts/login', redirect_field_name='')
+@login_required(login_url="/accounts/login", redirect_field_name="")
 def upload(request, user_id=None):
     form = VideoForm(request.POST or None, request.FILES or None)
-    if request.method == 'POST':
+    if request.method == "POST":
         if form.is_valid():
             obj = form.save(commit=False)
             obj.uploaded_by = request.user
             obj.save()
-            # path = f'{settings.MEDIA_ROOT}"/frames/"{str(obj.uploaded_by.id)}'
-            path = f'{settings.MEDIA_ROOT}/frames/{str(obj.uploaded_by.id)}/{os.path.basename(obj.videofile.name)}'
+            path = f"{settings.MEDIA_ROOT}/frames/{str(obj.uploaded_by.id)}/{os.path.basename(obj.videofile.name)}"
             isExist = os.path.exists(path)
             if not isExist:
                 os.makedirs(path)
@@ -40,54 +37,53 @@ def upload(request, user_id=None):
             # create thumbnail and add it to db
             # thumnailFrame = summary.get_video_duration()[1] // 2
             thumnailFrame = "1"
-            thumbData = open(os.path.join(path, f'frame{thumnailFrame}.jpg'), 'rb')
+            thumbData = open(os.path.join(path, f"frame{thumnailFrame}.jpg"), "rb")
             thumbFile = File(thumbData)
-            obj.thumbnail.save('thumbnail.jpg', thumbFile)
+            obj.thumbnail.save("thumbnail.jpg", thumbFile)
             form = VideoForm()
-            return redirect('scanline', obj.id)
-    return render(request, 'dashboard/upload.html', {"form": form})
+            return redirect("scanline", obj.id)
+    return render(request, "dashboard/upload.html", {"form": form})
 
 
-@login_required(login_url='/accounts/login', redirect_field_name='')
+def isExist(path) -> None:
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+@login_required(login_url="/accounts/login", redirect_field_name="")
 def scanline(request, video_id):
     video = get_object_or_404(Video, pk=video_id)
     if video.uploaded_by != request.user:
-        return HttpResponseNotFound('<h1>Not authenticated</h1>')
+        return HttpResponseNotFound("<h1>Not authenticated</h1>")
     else:
-        if request.method == 'POST':
-            scanlineValue = request.POST['scanline_slider']
-            # summary = SmrtSummary(str(video.videofile.path),
-            #                       f'{settings.MEDIA_ROOT}/frames/{str(video.uploaded_by.id)}')
-            summary = SmrtSummary(str(video.videofile.path),
-                                  f'{settings.MEDIA_ROOT}/frames/{str(video.uploaded_by.id)}/{os.path.basename(video.videofile.name)}')
+        if request.method == "POST":
+            scanlineValue = request.POST["scanline_slider"]
 
-            # f'{settings.MEDIA_ROOT}/frames/{str(video.uploaded_by.id)}/{os.path.basename(video.videofile.name)}'
-            
-            # path = f'{settings.MEDIA_ROOT}/frames/{str(video.uploaded_by.id)}/cropped'
-            path = f'{settings.MEDIA_ROOT}/frames/{str(video.uploaded_by.id)}/{os.path.basename(video.videofile.name)}/cropped'
+            summary = SmrtSummary(
+                str(video.videofile.path),
+                f"{settings.MEDIA_ROOT}/frames/{str(video.uploaded_by.id)}/{os.path.basename(video.videofile.name)}",
+            )
+
+            path = f"{settings.MEDIA_ROOT}/frames/{str(video.uploaded_by.id)}/{os.path.basename(video.videofile.name)}/cropped"
             isExist = os.path.exists(path)
             if not isExist:
                 os.makedirs(path)
             summary.create_summary(int(scanlineValue))
             # create video_summary and add it to db
-            smrtsummaryData = open(os.path.join(path, "summary.png"), 'rb')
+            smrtsummaryData = open(os.path.join(path, "summary.png"), "rb")
             smrtsummaryFile = File(smrtsummaryData)
-            video.video_summary.save('smrtsummary.jpg', smrtsummaryFile)
-            return redirect('video_summary', video_id)
+            video.video_summary.save("smrtsummary.jpg", smrtsummaryFile)
+            return redirect("video_summary", video_id)
         else:
-            context = {
-                'video': video
-            }
-            return render(request, 'dashboard/scanline.html', context)
+            context = {"video": video}
+            return render(request, "dashboard/scanline.html", context)
 
 
-@login_required(login_url='/accounts/login', redirect_field_name='')
+@login_required(login_url="/accounts/login", redirect_field_name="")
 def video_summary(request, video_id):
     video = get_object_or_404(Video, pk=video_id)
     if video.uploaded_by != request.user:
-        return HttpResponseNotFound('<h1>Not authenticated</h1>')
+        return HttpResponseNotFound("<h1>Not authenticated</h1>")
     else:
-        context = {
-            'video_summary': video.video_summary
-        }
-    return render(request, 'dashboard/video_summary.html', context)
+        context = {"video_summary": video.video_summary}
+    return render(request, "dashboard/video_summary.html", context)
